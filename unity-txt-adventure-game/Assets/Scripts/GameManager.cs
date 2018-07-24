940e4eafffc9b628f3ce0d4038c6e39c3ceaddec
Oskar Kogut <kogutoskar123@gmail.com>
2018-07-24 16:53:27 +0200

    Added State.cs and GameManager.cs

---
+++ unity-txt-adventure-game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField]
+    private Text storyText;
+
+    [SerializeField]
+    private Text answerAText;
+
+    [SerializeField]
+    private Text answerBText;
+
+    [SerializeField]
+    private State startingState;
+
+    private State currentState;
+
+	void Start () {
+        currentState = startingState;
+        storyText.text = currentState.GetStoryText();
+        answerAText.text = currentState.GetAnswerAText();
+        answerBText.text = currentState.GetAnswerBText();
+	}
+
+	void Update () {
+		
+	}
+}
---
+++ unity-txt-adventure-game/Assets/Scripts/State.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject {
+    [TextArea(14, 10)]
+    [SerializeField]
+    private string mStoryText;
+
+    [TextArea(14, 10)]
+    [SerializeField]
+    private string mAnswerAText;
+
+    [TextArea(14, 10)]
+    [SerializeField]
+    private string mAnswerBText;
+
+    public string GetStoryText()
+    {
+        return mStoryText;
+    }
+
+    public string GetAnswerAText()
+    {
+        return mAnswerAText;
+    }
+
+    public string GetAnswerBText()
+    {
+        return mAnswerBText;
+    }
+}
